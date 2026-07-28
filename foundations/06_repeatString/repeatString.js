2c9855847df513f9c13b8d11e394ba6e0e7ed719
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-27 21:47:15 -0500

    Outputs repeating strings as a custom function. Returns ERROR if negative.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+let input = "";
+let output = "";
+let count = 0;
 
-};
+const repeatString = function(input, count) {
+output = ""
+if (count < 0) {
+    return "ERROR";
+} else {
+    for (i = 0; i < count; i++) {
+    output = output + input;
+    }
+    return output;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
