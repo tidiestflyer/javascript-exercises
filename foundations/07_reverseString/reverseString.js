0940ec8c98fc600d0e32579b8e1b8f30c96ec7af
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-28 14:58:40 -0500

    Made a function that reverses a string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+let input = "";
+let output = "";
+let count = 0;
 
+const reverseString = function(input) {
+    output = "";
+    for (count = input.length - 1; count >= 0; count--) {
+        output = output + input[count];
+    }
+return output;
 };
 
 // Do not edit below this line
