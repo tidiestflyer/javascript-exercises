3fcfff3153e9fd79c5ccdddd330b44544595e62c
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-29 15:29:51 -0500

    Built a function that removes elements of an array with arugments. This took a couple days to wrap my head around. Not because it was difficult, but because I was trying to over complicate the process.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-};
+let arrayToModify = "";
 
+const removeFromArray = function() {
+    for (i = 0; i < (arguments.length - 1); i++) {
+        let toDelete = arguments[i + 1];
+        console.log(toDelete);
+
+        for (i1 = 0; i1 < (arguments[0].length); i1++) {
+            arrayToModify = arguments[0];
+            console.log(arrayToModify[i1] + " " + toDelete);
+            if (arrayToModify[i1] === toDelete) {
+                console.log("Matched, delete this statement");
+                arrayToModify.splice([i1], 1);
+                i1 = 0;
+            } else {
+                console.log("If did not match...")
+            }
+        }
+    }
+    console.log(arrayToModify);
+    return arrayToModify;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
