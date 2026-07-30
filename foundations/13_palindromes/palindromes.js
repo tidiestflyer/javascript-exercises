c30910a5064b43f8eda74c5754cd94ec119a5e4c
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-30 11:08:01 -0500

    Built a function that detects wither a string is a palindrome. Function ignores capalization, punctuation, and spaces.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,27 @@
 const palindromes = function () {
+    let forward = arguments[0].toLowerCase();
+    let backward = "";
+    let characterToCheck = "";
 
+    //remove these characters
+    forward = forward.replaceAll(",", "");
+    forward = forward.replaceAll(".", "");
+    forward = forward.replaceAll("!", "");
+    forward = forward.replaceAll("?", "");
+    forward = forward.replaceAll(" ", "");
+
+    for (i = forward.length; i > 0; i--) {
+        characterToCheck = forward.substr(i - 1, 1);
+        backward = backward + characterToCheck;
+        console.log(backward);
+    }
+
+    console.log(`Loop is completed. Now does "${forward}" equal "${backward}" ?`);
+    if (forward == backward) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
