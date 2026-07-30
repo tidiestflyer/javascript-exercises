6b07ed99ca5195db7e9017b7e403e9519aab2d14
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-30 13:14:29 -0500

    Created a function that grabs all titles from a list of objects and returns them as a new array. Before I had to go on a whole research quest my console.log would keep returning 'object Object', and I just did not understand till I realized it was an array of objects and not just a simple array.

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
 const getTheTitles = function() {
-
+    let books = arguments[0];
+    let outputArray = [];
+    for (i = 0; i < books.length; i++) {
+        outputArray[i] = books[i].title;
+    }
+    return outputArray
 };
 
 // Do not edit below this line
