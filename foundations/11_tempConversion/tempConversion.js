869071c4b48224facbf6c494070a4c2f27d79beb
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-29 23:55:50 -0500

    Created a function that converts celsius to fahrenheit and vice versa.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
 const convertToCelsius = function() {
+  // formula for conversion (32°F − 32) × 5/9 = 0°C
+  let input = arguments[0];
+  let output = (input - 32) * 5 / 9;
+  output = Math.round(output * 10) / 10;
+  console.log(output);
+  return output;
 };
 
 const convertToFahrenheit = function() {
+  // formula for conversion (32°C * 9/5) + 32 = 89.6°F
+  let input = arguments[0]
+  let output = (input * 9 / 5) + 32;
+  output = Math.round(output * 10) / 10;
+  console.log(output);
+  return output;
 };
 
 // Do not edit below this line
