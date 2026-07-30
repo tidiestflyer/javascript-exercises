e5a1df3559122001fc17539d14ceb7980294221f
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-30 11:40:25 -0500

    Created a function that produces a value from the fibonacci sequence at a specific index. Returns "OOPS" if the input is negative.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
 const fibonacci = function() {
+    let fibo1 = 1;
+    let fibo2 = 0;
+    let fiboOutput = 0;
 
+    if (arguments[0] < 0) {
+        return "OOPS";
+    }
+    //In my head I'm envisioning the values sliding as they are used. fiboOutput => fibo2 => fibo1. fiboOutput is the result at the given sequence.
+    for (i = 0; i < arguments[0]; i++) {
+            fiboOutput = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fiboOutput;
+    }
+    return fiboOutput;
 };
 
 // Do not edit below this line
