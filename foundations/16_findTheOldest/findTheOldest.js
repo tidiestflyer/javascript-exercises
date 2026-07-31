ec09fe74543638384a67c856cacaa35d4687f0fd
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-07-30 20:47:34 -0500

    Created a function that looks through a series of objects and returns the oldest person. If the person does not have a death year, then it uses the current year to subtract their age instead, IE, the person is still alive.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
 const findTheOldest = function() {
+    let peoples = arguments[0];
+    let oldestPerson = "";
+    let largestAge = 0;
+    let ageToCheck = 0;
 
+    let yearOfBirth = 0;
+    let yearOfDeath = 0;
+
+    let currentYear = 2027;
+
+    for (i = 0; i < peoples.length; i ++) {
+        yearOfBirth = peoples[i].yearOfBirth;
+        /*If a person is still alive (has no death year), then the top range for their age calc is the current year.
+        For ease of calculation, this is considered their death year. */
+        if (peoples[i].yearOfDeath == null || peoples[i].yearOfDeath == undefined) {
+            console.log("No Death year");
+            yearOfDeath = currentYear;
+        } else {
+            yearOfDeath = peoples[i].yearOfDeath;
+        }
+        ageToCheck = yearOfDeath - yearOfBirth;
+
+        if (largestAge < ageToCheck) {
+            largestAge = ageToCheck;
+            oldestPerson = peoples[i];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
