4b2bf3bd2778a47adae1a15c08d4f0507b26319d
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-08-04 15:23:23 -0500

    Simplified the problem by using the built in array functions to reorder the string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,14 +1,8 @@
-let input = "";
-let output = "";
-let count = 0;
-
 const reverseString = function(input) {
-    output = "";
-    for (count = input.length - 1; count >= 0; count--) {
-        output = output + input[count];
-    }
-return output;
-};
+    array = input.split("").reverse();
+    backToString = array.join("");
+    return backToString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
