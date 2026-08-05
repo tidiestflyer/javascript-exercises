e54a2767e12aa8b19e17da94ffa596e391785264
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-08-05 18:44:17 -0500

    Simplified leapYears function.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,19 +1,8 @@
-const leapYears = function() {
-    let year = arguments[0];
-    let resultOf4 = year / 4;
-    let resultOf400 = year / 400;
-    let resultOf100 = year / 100;
-
-    let canDivide4 = resultOf4 == Math.floor(resultOf4);
-    let canDivide400 = resultOf400 == Math.floor(resultOf400);
-    let canDivide100 = resultOf100 == Math.floor(resultOf100);
-
-    if ((canDivide400 == false && canDivide100 == false && canDivide4 == true) || (canDivide400 == true && canDivide100 == true && canDivide4 == true)) {
-        return true;
-    } else {
-        return false;
-    }
-};
+const leapYears = function(year) {
+    if (year % 4 == 0 && year % 100 != 0) return true;
+    else if (year % 400 == 0) return true;
+    else return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
