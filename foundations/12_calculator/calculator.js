25e1635c08bd69f93bc041d07a208c55bf3d5275
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-08-11 12:41:54 -0500

    Remade the function that returns a factorial.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -24,7 +24,12 @@ const power = function(number, power) {
   return number ** power;
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  numbers = [];
+  for(i = number; i > 0; i--) {
+    numbers.push(i);
+  }
+  return numbers.reduce((acc, curr) => acc *= curr, 1);
 };
 
 // Do not edit below this line
