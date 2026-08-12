577caeafeb0868172ee2a98f20258a54a52bdfc3
tidiestflyer <35712857+tidiestflyer@users.noreply.github.com>
2026-08-11 21:43:26 -0500

    Made the function more readable, and now uses filter

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,27 +1,12 @@
-const palindromes = function () {
-    let forward = arguments[0].toLowerCase();
-    let backward = "";
-    let characterToCheck = "";
+const palindromes = function (phrase) {
+    const allowedChar = "abcdefghijklmnopqrstuvwxyz1234567890";
+    
+    splitArray = phrase.toLowerCase().split("");
+    splitArray = splitArray.filter((char) => allowedChar.includes(char));
+    forwardPhrase = splitArray.join("");
+    backwardPhrase = splitArray.reverse().join("");
 
-    //remove these characters
-    forward = forward.replaceAll(",", "");
-    forward = forward.replaceAll(".", "");
-    forward = forward.replaceAll("!", "");
-    forward = forward.replaceAll("?", "");
-    forward = forward.replaceAll(" ", "");
-
-    for (i = forward.length; i > 0; i--) {
-        characterToCheck = forward.substr(i - 1, 1);
-        backward = backward + characterToCheck;
-        console.log(backward);
-    }
-
-    console.log(`Loop is completed. Now does "${forward}" equal "${backward}" ?`);
-    if (forward == backward) {
-        return true;
-    } else {
-        return false;
-    }
+    if(forwardPhrase == backwardPhrase) return true; return false;
 };
 
 // Do not edit below this line
